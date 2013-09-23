5a3d3de0631350a04b00678c77ce75a745bd7d04
Bryan Smith <zarboz@gmail.com>
2013-09-23 07:59:25 -0500

    c;ean this soon
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 13 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#13 SMP PREEMPT Mon Sep 23 07:44:50 CDT 2013"
+#define LINUX_COMPILE_BY "zarboz"
+#define LINUX_COMPILE_HOST "Cerebro"
+#define LINUX_COMPILER "gcc version 4.7.4 20130626 (prerelease) (Linaro GCC 4.7-2013.07) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.10-g445d072"
