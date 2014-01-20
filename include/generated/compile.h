18d0825374910cf239964c05ff83f227b4975240
Bryan Smith <zarboz@gmail.com>
2014-01-19 19:31:14 -0700

    updating gcc 4.8
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Mon Sep 23 10:09:39 CDT 2013"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Jan 19 18:30:37 MST 2014"
 #define LINUX_COMPILE_BY "zarboz"
 #define LINUX_COMPILE_HOST "Cerebro"
-#define LINUX_COMPILER "gcc version 4.7.4 20130626 (prerelease) (Linaro GCC 4.7-2013.07) "
+#define LINUX_COMPILER "gcc version 4.8.3 20131202 (prerelease) (Linaro GCC 4.8-2013.12) "
